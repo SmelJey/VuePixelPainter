de04f9c2e58560656e0155ddb6fe1e01d2d53a60
Юрий Михалев <mixalevuriy@gmail.com>
2019-12-25 13:49:22 +1000

    Add user info to profile

--- pixel_painter/src/components/Profile.vue
+++ pixel_painter/src/components/Profile.vue
@@ -2,6 +2,36 @@
 	<div class="main">
 		<Navbar/>
 		<div class="shadow"></div>
+		<div class="modal is-active" v-if="showModal">
+			<div class="modal-background"></div>
+			<div class="modal-card">
+				<header class="modal-card-head">
+					<p class="modal-card-title">Change personal information</p>
+					<button class="delete" aria-label="close" v-on:click="closeModal"></button>
+				</header>
+				<section class="modal-card-body">
+					<div class="level">
+						<input id="email" class="input" type="text" placeholder="Email" v-model="newMeta['email']"/>
+						<button class="button is-success" v-on:click="changeEmail">Change Email</button>
+					</div>
+
+					<div class="level">
+						<input id="pswd" class="input" type="password" placeholder="Password" v-model="newMeta['password']"/>
+						<button class="button is-success" v-on:click="changePass">Change password</button>
+					</div>
+
+
+					<input id="fname" class="input" type="text" placeholder="First Name"  v-model="newMeta['first_name']"/>
+					<input id="sname" class="input" type="text" placeholder="Second Name" v-model="newMeta['second_name']"/>
+					<input id="age" class="input" type="text" placeholder="Age" v-model="newMeta['age']"/>
+					<input id="country" class="input" type="text" placeholder="Country" v-model="newMeta['country']"/>
+					<input id="vk" class="input" type="text" placeholder="VK" v-model="newMeta['vk_profile']"/>
+				</section>
+				<footer class="modal-card-foot">
+					<button class="button is-success" v-on:click="saveMeta">Save</button>
+				</footer>
+			</div>
+		</div>
 		<section class="section">
 			<div class="media">
 				<div class="media-left">
@@ -11,14 +41,19 @@
 				</div>
 				<div class="media-content">
 					<p class="title is-2">{{ accountName }}</p>
-					<p class="subtitle is-3">{{ accountMeta }}</p>
+					<p class="subtitle is-3">{{ [accountMeta['first_name'], accountMeta['second_name']].filter(Boolean).join(" ") }}</p>
 				</div>
 				<div class="media-right">
 				</div>
 			</div>
 		</section>
-		<section class="section">
-			<textarea class="textarea is-fullwidth"></textarea>
+		<section class="section has-text-left">
+			<p class="title">Personal information: </p>
+			<p class="subtitle">{{ accountMeta['age'] ? 'Age: ' + accountMeta['age'] : ''}}</p>
+			<p class="subtitle">{{ accountMeta['email'] ? 'Email: ' + accountMeta['email'] : ''}}</p>
+			<p class="subtitle">{{ accountMeta['country'] ? 'Country: ' + accountMeta['country'] : ''}}</p>
+			<p class="subtitle">{{ accountMeta['vk_profile'] ? 'Vk:  @' + accountMeta['vk_profile'] : ''}}</p>
+			<button class="button" v-on:click="showModal = true"> Change your personal information </button>
 		</section>
 		<section class="section" v-if="imageList">
 			<span class="container" v-for="image in getImages" v-bind:key="image.id">
@@ -48,12 +83,22 @@ export default {
     data: function(){
         return {
             accountPic: 'https://bulma.io/images/placeholders/128x128.png',
-            accountName: this.$store.getters.getUsername,
-            accountMeta: '@None',
-            imageList: null,
-            offset: 0,
+            accountName: '',
+            accountMeta: '',
+			newMeta: {
+				first_name: '',
+				second_name: '',
+				age: 0,
+				country: '',
+				vk_profile: ''
+			},
+			imageList: null,
+			offset: 0,
             numberOfPic: 50,
-			isRequired: false
+			isRequired: false,
+			showModal: false,
+			error: "",
+			reqCount: 0
         }
     },
     computed: {
@@ -67,24 +112,24 @@ export default {
     },
     methods: {
 		requestImages() {
-			axios.post('/get?token=' + this.$cookies.get('token'))
-							.then((response) => {
-								console.log(response.data);
-								let list = []
-								if (response.data["status"] === "OK") {
-									for (let i = 0; i < response.data["items"].length; ++i) {
-										list.push({id: i, url: response.data["items"][i].data});
-									}
-									this.imageList = list;
-									this.drawImageOnCanvas()
-								} else {
-									this.goToAuth();
-								}
-								this.isRequired = true;
-							})
-							.catch((error) => {
-								console.log(error);
-							});
+			axios.get('/get?token=' + this.$cookies.get('token'))
+				.then((response) => {
+					console.log(response.data);
+					let list = []
+					if (response.data["status"] === "OK") {
+						for (let i = 0; i < response.data["items"].length; ++i) {
+							list.push({id: i, url: response.data["items"][i].data});
+						}
+						this.imageList = list;
+						this.drawImageOnCanvas()
+					} else {
+						this.goToAuth();
+					}
+					this.isRequired = true;
+				})
+				.catch((error) => {
+					console.log(error);
+				});
 		},
 		changeProfile () {
 
@@ -99,10 +144,114 @@ export default {
 					canvas.drawImage(image, 0, 0)
 				}
 			}
-        }
+        },
+		requestMeta(){
+			if (this.$cookies.get('token') !== null){
+				console.log("requested");
+				Axios.get('http://localhost:8080/account/get?token=' + this.$cookies.get('token'))
+					.then((response) => {
+
+						console.log(response.data);
+						if (response.data['status'] === 'OK') {
+							this.accountName = response.data['login'];
+							this.accountMeta = response.data;
+
+							for (let i in response.data) {
+								if (Object.prototype.hasOwnProperty.call(response.data, i)) {
+									this.accountMeta[i] = response.data[i];
+									this.newMeta[i] = this.accountMeta[i];
+								}
+							}
+							console.log(this.newMeta);
+						} else if (response.data['status'] === 'INVALID_TOKEN') {
+							this.$router.push({name: 'Auth'})
+						}
+					}).catch((error) => {
+						console.log("It was me MArio!, requested")
+						console.log(error);
+					})
+			} else {
+				this.$router.push({name: 'Auth'})
+			}
+		},
+		async saveMeta() {
+			console.log(this.newMeta);
+			for (let key in this.newMeta) {
+				if (['status', 'login', 'password', 'email'].includes(key))
+					continue;
+				if (!Object.hasOwnProperty.call(this.newMeta, key))
+					continue;
+				console.log([key, this.newMeta[key]]);
+				this.reqCount++;
+				if (this.$cookies.get('token') !== null){
+					Axios.post('http://localhost:8080/account/edit?&field=' + key + '&value='
+						+ this.newMeta[key] + '&token=' + this.$cookies.get('token'))
+						.then((response) => {
+							console.log(response.data);
+							console.log([key, this.newMeta[key]]);
+							if (response.data['status'] === 'INVALID_TOKEN') {
+								if (this.$router.currentRoute.name !== 'Auth')
+									this.$router.push({name: 'Auth'})
+							}
+							this.reqCount--;
+							if (this.reqCount <= 0) {
+								this.requestMeta();
+							}
+						}).catch((error) => {
+							console.log(error);
+						})
+
+				} else {
+					this.$router.push({name: 'Auth'})
+				}
+				await new Promise(r => setTimeout(r, 100));
+			}
+		},
+		changePass() {
+			Axios.post('http://localhost:8080/account/edit?&field=' + 'password' + '&value='
+				+ this.newMeta['password'] + '&token=' + this.$cookies.get('token'))
+				.then((response) => {
+					console.log(response.data);
+					if (response.data['status'] === 'FAIL') {
+						this.error = "Incorrect pass"
+					} else {
+						this.error = "";
+						if (this.$router.currentRoute.name !== 'Auth')
+							this.$router.push({name: 'Auth'})
+					}
+				}).catch((error) => {
+					console.log(error);
+				})
+		},
+		changeEmail() {
+			Axios.post('http://localhost:8080/account/edit?&field=' + 'email' + '&value='
+					+ this.newMeta['email'] + '&token=' + this.$cookies.get('token'))
+					.then((response) => {
+						console.log(response.data);
+						if (response.data['status'] === 'FAIL') {
+							this.error = "Incorrect email"
+						} else {
+							this.error = "";
+							if (this.$router.currentRoute.name !== 'Auth')
+								this.$router.push({name: 'Auth'})
+						}
+					}).catch((error) => {
+				console.log(error);
+			})
+		},
+		closeModal() {
+			this.showModal = false;
+			this.newMeta.clear();
+			for (let i in this.accountMeta) {
+				if (Object.prototype.hasOwnProperty.call(this.accountMeta, i)) {
+					this.newMeta.set(i, this.accountMeta[i]);
+				}
+			}
+		}
 	},
     mounted() {
 		this.requestImages();
+		this.requestMeta();
 		console.log(2);
 	}	
 
